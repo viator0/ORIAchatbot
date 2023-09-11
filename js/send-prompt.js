64602478fb36ca9e85a30b1bddb0080dd9af967f
Cahit <103262027+cahitdogan@users.noreply.github.com>
2023-09-11 15:02:25 +0300

    Add files via upload

--- js/send-prompt.js
+++ js/send-prompt.js
@@ -1,13 +1,46 @@
-const examplePromptsContainer = document.querySelector("#example-prompts");
+const chatBalloonsSection = document.querySelector("#chat-balloons-section");
 const promptInput = document.querySelector("#prompt-input");
+const examplePromptsContainer = document.querySelector("#example-prompts");
+const examplePrompts = document.querySelectorAll("#example-prompts article");
 
 sendButton.addEventListener('click', () => {
-
     if (promptInput.value.trim() !== "") {
-        hideExamplePrompts();
+        examplePromptsContainer.style.display = "none";
+        chatBalloonsSection.style.display = "flex";
+        userPromptFunction("text");
+        aiResponseFunction();
     }
 });
 
-function hideExamplePrompts() {
-    examplePromptsContainer.style.display = "none";
+examplePrompts.forEach((examplePrompt) => {
+    examplePrompt.addEventListener('click', () => {
+        examplePromptsContainer.style.display = "none";
+        chatBalloonsSection.style.display = "flex";
+        userPromptFunction("example", examplePrompt.innerHTML);
+        aiResponseFunction();
+    });
+})
+
+ 
+function userPromptFunction(inputType, exampleOrder) {
+    const userPrompt = document.createElement("article");
+    userPrompt.classList.add("user-prompt");
+    userPrompt.classList.add("balloons");
+    switch (inputType) {
+        case "text":
+            userPrompt.textContent = promptInput.value;
+            break;
+        case "example":
+            userPrompt.textContent = exampleOrder;
+    }
+    promptInput.value = "";
+    chatBalloonsSection.appendChild(userPrompt);
+}
+
+function aiResponseFunction() {
+    const aiResponse = document.createElement("article");
+    aiResponse.classList.add("ai-response");
+    aiResponse.classList.add("balloons");
+    aiResponse.textContent = "AI Response Text";
+    chatBalloonsSection.appendChild(aiResponse);
 }
